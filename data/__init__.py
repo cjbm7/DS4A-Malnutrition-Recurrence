b3da186e8615da13bd8bef9f32aca2da34ea07c5
cjbm7 <afterone007@gmail.com>
2021-09-02 18:09:51 -0500

    Indicadores Regionales

--- data/__init__.py
+++ data/__init__.py
@@ -7,10 +7,12 @@ dbase = os.path.join(basedir, 'db','predictions.db')
 modelpk = os.path.join(basedir, 'model','CD_his_RF3.pickle') 
 
 dpto_mpio = os.path.join(basedir, 'core','dpto_mpio.parquet')
+
+#tomas_pq = os.path.join(basedir, 'core','prediccion_tomas_50.parquet') #Vista regional y seguimiento nutricional
 tomas_pq = os.path.join(basedir, 'core','tomas_max_500_act.parquet') #Vista regional y seguimiento nutricional
 
 tom3000_pq = os.path.join(basedir, 'plots','tomas_3000_act.parquet')  #Scatterplot
 boxplots_pq = os.path.join(basedir, 'plots','data_frame_to_boxplot.parquet')  
-socio_eda_mpios = os.path.join(basedir, 'plots','sociodemo_EDA_mcpio.parquet') #heatmaps sociodemo
+socio_eda_mpios = os.path.join(basedir, 'plots','sociodemo_mpio_act.parquet') #heatmaps sociodemo
 geojson_mpios = os.path.join(basedir, 'plots', 'co_2018_MGN_MPIO_POLITICO.geojson') #mapas
 points_z = os.path.join(basedir, 'plots', 'puntos_peso_talla.csv') #Lineas gias 
